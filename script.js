5c7bc60b529f83087beddbabe3d6190a2991a1bf
Brian Huh <brianhuh01@gmail.com>
2021-04-03 23:54:57 -0400

    not working now but committing before switching to a working branch

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ let myLibrary = [
 function Book(author, title, page, read) {
     this.author = author
     this.title = title
-    this.page = page
+    this.page = page + " pages"
     this.read = read
+    console.log(author)
     // this.info = function() {
     //     return (`${title} by ${author}, ${page}, ${read}`)
     // }
@@ -33,9 +34,49 @@ function addBookToLibrary(a, b, c, d) {
 }
 
 
-//addBookToLibrary(a, b, c, d)
+// addBookToLibrary("a", "b", 'c', 'd')
 // addBookToLibrary(q,w,e,r)
-console.log(myLibrary)
+
+const libGen = document.querySelector('.library-generated')
 
 
-console.table(myLibrary)
+
+for (let i=0; i<myLibrary.length; i++) {
+  const added = document.createElement('div')
+  const author = document.createElement('div')
+  const title = document.createElement('div')
+  const page = document.createElement('div')
+  const read = document.createElement('div')
+
+  added.classList.add('added')
+  author.classList.add('author')
+  title.classList.add('title')
+  page.classList.add('page')
+  read.classList.add('read')
+  
+  author.innerHTML += `Author: ${myLibrary[i].author}`
+  title.innerHTML += `Title: ${myLibrary[i].title}`
+  page.textContent += `Page: ${myLibrary[i].page}`
+  read.textContent += `Read: ${myLibrary[i].read}`
+  libGen.appendChild(added)
+  added.appendChild(author)
+  added.appendChild(title)
+  added.appendChild(page)
+  added.appendChild(read)
+
+}
+
+const button = document.querySelector('#submit')
+const author = document.querySelector('input[name="author"]')
+const title = document.querySelector('input[name="title"]')
+const page = document.querySelector('input[name="page"]')
+const read = document.querySelector('input[name="read"]')
+const notread = document.querySelector('input[name="notread"]')
+
+button.addEventListener("click", () =>
+Book(author.textContent, title.textContent, page.textContent, read.textContent)
+)
+// console.log(myLibrary)
+
+
+// console.table(myLibrary)
